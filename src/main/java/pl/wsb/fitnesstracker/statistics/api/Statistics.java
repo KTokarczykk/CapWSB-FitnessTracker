7cfe7b5c7f7b8eb7b7dba55920afc46765fb3234
Mateusz Prostak <matthew43000@gmail.com>
2025-11-09 09:52:24 +0100

    Fixed relations

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
 @Table(name = "Statistics")
@@ -17,9 +18,9 @@ public class Statistics {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
     @OneToOne
-    private Long userId;
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
     @Column(nullable = false)
     private int totalTrainings;
@@ -30,8 +31,8 @@ public class Statistics {
     @Column
     private int totalCaloriesBurned;
 
-    public Statistics(Long userId, int totalTrainings, double totalDistance, int totalCaloriesBurned) {
-        this.userId = userId;
+    public Statistics(User user, int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+        this.user = user;
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
