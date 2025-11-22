01757aa9d3f57dc61459ad8dd0789556f9f9ba7d
Krystian Tokarczyk <kystian.t200@poczta.fm>
2025-11-22 15:10:10 +0100

    Event

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,38 +1,41 @@
 package pl.wsb.fitnesstracker.event;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
-@Table(name = "event")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Event")
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
 
-    @Column(name = "description", columnDefinition = "TEXT")
+    @Column(nullable = false)
     private String description;
 
-    @Column(name = "start_time", nullable = false)
-    private LocalDateTime startTime;
+    @Column(nullable = false)
+    private Date startTime;
 
-    @Column(name = "end_time", nullable = false)
-    private LocalDateTime endTime;
+    @Column(nullable = false)
+    private Date endTime;
 
-    @Column(name = "country")
+    @Column(nullable = false)
     private String country;
 
-    @Column(name = "city")
+    @Column(nullable = false)
     private String city;
 
+
+    public Event(String name, String description, Date startTime, Date endTime, String country, String city) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
