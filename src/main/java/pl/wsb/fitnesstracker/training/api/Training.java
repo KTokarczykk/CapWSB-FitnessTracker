72a7c8b035288e7638e8120129e4e304b9c1a57a
Mateusz Prostak <matthew43000@gmail.com>
2025-11-22 15:11:12 +0100

    Created Trainings and Workout_Session

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -6,21 +6,41 @@ import pl.wsb.fitnesstracker.user.api.User;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Trainings")
 @Getter
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @Column(nullable=false)
     private Date startTime;
 
+    @Column(nullable=false)
     private Date endTime;
 
+    @Column(nullable=false)
     private ActivityType activityType;
 
+    @Column(nullable=false)
     private double distance;
 
+    @Column(nullable=false)
     private double averageSpeed;
 
     public Training(
